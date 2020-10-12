3add52d1568daed297aeb9cf7dfb5fdea737833b
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2020-10-12 01:30:40 -0300

    header services

--- src/app/@componets/header/header.component.ts
+++ src/app/@componets/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'src/app/services/header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  title$ = this.headerService.title;
+
+  constructor(private headerService: HeaderService) { }
+
+
 
   ngOnInit(): void {
   }
---
+++ src/app/services/header.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  title: BehaviorSubject<string>
+
+  constructor() {
+
+    this.title= new BehaviorSubject("gestion de ususarios")
+
+
+  }
+}
